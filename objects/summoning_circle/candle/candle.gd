2a32c1fe4b06522842d396f134571defbc8f52b1
gizmolo <jacob.wallace@utexas.edu>
2024-04-13 16:44:09 -0500

    candles activate

---
+++ objects/summoning_circle/candle/candle.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func activate_candle():
+	$CandleLightSprite.frame = 1
+
+func deactivate_candle():
+	$CandleLightSprite.frame = 0
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		activate_candle()
