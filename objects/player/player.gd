c8849b85286d4ddbf93b6eec5e58567b64431e98
gizmolo <jacob.wallace@utexas.edu>
2024-04-13 11:35:19 -0500

    summoning input

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -9,6 +9,9 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("Shoot"):
 		shoot_bullet()
+	
+	if Input.is_action_pressed("Summon"):
+		summon_tick()
 
 	var direction := Vector2(Input.get_axis("Left", "Right"), Input.get_axis("Up", "Down"))
 	velocity = direction * SPEED
@@ -18,6 +21,13 @@ func _physics_process(delta: float) -> void:
 # Called when colliding with something for any reason.
 func _collision(other: PhysicsBody2D) -> void:
 	pass
+
+func summon_tick():
+	var new_summoning_dust = summoning_dust.instantiate()
+	var summoning_dust_pos = self.get_position()
+	#bullet_pos.y -= 50
+	new_summoning_dust.set_position(summoning_dust_pos)
+	self.get_parent().add_child(new_summoning_dust)
 
 func shoot_bullet():
 	var new_bullet = bullet_scene.instantiate()
