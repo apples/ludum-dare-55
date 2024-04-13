dcd7245cfa2e3e1bcccdc7b6ffba41722e4cb542
gizmolo <jacob.wallace@utexas.edu>
2024-04-13 17:39:48 -0500

    summoning ink only matters when you start a sigil sequence

--- objects/summoning_circle/summoning_circle.gd
+++ objects/summoning_circle/summoning_circle.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 var player_ref: CharacterBody2D
+var sigil_sequence_active = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Candle1.summoning_circle_ref = self
+	$Candle2.summoning_circle_ref = self
+	$Candle3.summoning_circle_ref = self
+	$Candle4.summoning_circle_ref = self
+	$Candle5.summoning_circle_ref = self
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +19,7 @@ func _process(delta):
 
 func out_of_juice():
 	print("yoinks!")
+	sigil_sequence_active = false
 	$Candle1.deactivate_candle()
 	$Candle2.deactivate_candle()
 	$Candle3.deactivate_candle()
