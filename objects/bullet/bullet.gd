fe2105436101ecfb49985722802609178aa170c0
xxAtrain223 <xxAtrain223@users.noreply.github.com>
2024-04-13 12:35:42 -0500

    Added BulletSpawner

--- objects/bullet/bullet.gd
+++ objects/bullet/bullet.gd
@@ -1,22 +1,26 @@
 extends Node2D
 
+class_name Bullet
+
 const SPEED = 12.0
-var diretion := Vector2(0, -1)
+var direction := Vector2(0, 1)
 var damage := 1
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	position += diretion * SPEED
+	transform = transform.translated_local(direction * SPEED)
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	print("Hit???")
 	if body.is_in_group("Enemy"):
 		body.hit(damage)
 		queue_free()
+		print("enemy")
 	elif body.is_in_group("Wall"):
 		queue_free()
+		print("wall")
 	else:
 		print("nope")
 	
