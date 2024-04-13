7a511ea3d0be13b446c818fe676cef61b8b697f6
WiredOverload <mike.everywhere@gmail.com>
2024-04-12 23:00:46 -0500

    Added very basic character objects

---
+++ objects/bullet/bullet.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+const SPEED = 3.0
+var diretion := Vector2(0, 1)
+var damage := 1
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	position += diretion * SPEED
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	print("Hit???")
+	if body.is_in_group("Enemy"):
+		body.hit(damage)
+		queue_free()
+	elif body.is_in_group("Wall"):
+		queue_free()
+	else:
+		print("nope")
+	
---
+++ objects/player/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+
+	if Input.is_action_just_pressed("Shoot"):
+		print("Bang")
+
+	var direction := Vector2(Input.get_axis("Left", "Right"), Input.get_axis("Up", "Down"))
+	velocity = direction * SPEED
+
+	move_and_slide()
+
+# Called when colliding with something for any reason.
+func _collision(other: PhysicsBody2D) -> void:
+	pass
