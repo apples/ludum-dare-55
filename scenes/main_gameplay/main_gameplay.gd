59eb7e9a7b58c527ba1b6bf5853ed83776ff51cf
Apples <2352020+apples@users.noreply.github.com>
2024-04-13 09:02:16 -0500

    Sample phase 2

--- scenes/main_gameplay/main_gameplay.gd
+++ scenes/main_gameplay/main_gameplay.gd
@@ -7,3 +7,7 @@ func _on_bouncing_character_body_2d_bounce(collision: KinematicCollision2D) -> v
 	#camera_shake.apply_impulse(Vector2.from_angle(randf_range(0, TAU)) * 2000)
 	camera_shake.rumble(50, 0.25)
 
+
+
+func _on_example_phase_phase_complete() -> void:
+	add_child(preload("res://scenes/main_gameplay/stage_phases/phase_2.tscn").instantiate())
