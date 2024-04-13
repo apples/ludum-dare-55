d194c5b14d6cbb105f01ef524538a2fd6d293774
Apples <2352020+apples@users.noreply.github.com>
2024-04-13 07:04:24 -0500

    Fixed state machine

--- objects/enemy/enemy.gd
+++ objects/enemy/enemy.gd
@@ -39,24 +39,24 @@ func __opt_func(n: StringName) -> Callable:
 	return Callable(self, n) if self.has_method(n) else Callable()
 func _add_state(state_name: String) -> void:
 	_states[state_name] = {
-		process = __opt_func("_state" + state_name + "_process"),
-		physics_process = __opt_func("_state" + state_name + "_physics_process"),
-		enter = __opt_func("_state" + state_name + "_enter"),
-		exit = __opt_func("_state" + state_name + "_exit") }
+		process = __opt_func("_state_" + state_name + "_process"),
+		physics_process = __opt_func("_state_" + state_name + "_physics_process"),
+		enter = __opt_func("_state_" + state_name + "_enter"),
+		exit = __opt_func("_state_" + state_name + "_exit") }
 	if _states[state_name].values().count(Callable()) == 4:
 		push_warning("State %s has no state functions! Typo?" % [state_name])
 		breakpoint
 func _goto(state_name: StringName) -> void:
-	if _current_state and _states[_current_state].exit: _states[_current_state].exit()
+	if _current_state and _states[_current_state].get("exit", Callable()): _states[_current_state].exit.call()
 	_current_state = state_name
 	if not _current_state: return
 	if not _current_state in _states: _add_state(_current_state)
-	if _current_state and _states[_current_state].enter: _states[_current_state].enter()
+	if _current_state and _states[_current_state].get("enter", Callable()): _states[_current_state].enter.call()
 func _process(delta: float) -> void:
 	if &"_always_process" in self: (self as Variant)._always_process(delta)
-	if _current_state and _states[_current_state].process: _states[_current_state].process(delta)
+	if _current_state and _states[_current_state].get("process", Callable()): _states[_current_state].process.call(delta)
 func _physics_process(delta: float) -> void:
 	if &"_always_physics_process" in self: (self as Variant)._always_physics_process(delta)
-	if _current_state and _states[_current_state].process: _states[_current_state].physics_process(delta)
+	if _current_state and _states[_current_state].get("physics_process", Callable()): _states[_current_state].physics_process.call(delta)
 #endregion
 
