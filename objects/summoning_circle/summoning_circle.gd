de9184628464b155a13bc42430af424ce51761d2
WiredOverload <mike.everywhere@gmail.com>
2024-04-14 12:08:39 -0500

    player can now switch colors

--- objects/summoning_circle/summoning_circle.gd
+++ objects/summoning_circle/summoning_circle.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var player_ref: CharacterBody2D
+var player_ref: Player
 var camera_shake_ref: CameraShake
 var sigil_sequence_active = false
 var current_sigil_sequence = []
@@ -28,13 +28,27 @@ func check_sigil_sequence():
 	print(current_sigil_sequence)
 	if current_sigil_sequence == [4,1,3,5,2]:
 		trigger_41352_star_sigil()
+	elif current_sigil_sequence == [1,2,3,4,5]:
+		trigger_12345_star_sigil()
+	elif current_sigil_sequence == [5,4,3,2,1]:
+		trigger_54321_star_sigil()
 	else:
 		load_failed_sigil_vfx()
 		camera_shake_ref.rumble(10, 1)
 
 func trigger_41352_star_sigil():
+	player_ref.current_element = Globals.Elements.WATER
 	player_ref.current_bullet_pattern = BulletSpawner.fire_circle
-	print("that's a star yo")
+	load_sigil_vfx()
+
+func trigger_12345_star_sigil():
+	player_ref.current_element = Globals.Elements.FIRE
+	player_ref.current_bullet_pattern = BulletSpawner.fire_three_arc_immediate
+	load_sigil_vfx()
+
+func trigger_54321_star_sigil():
+	player_ref.current_element = Globals.Elements.VEGANS
+	player_ref.current_bullet_pattern = BulletSpawner.fire_one_straight
 	load_sigil_vfx()
 
 func load_sigil_vfx():
