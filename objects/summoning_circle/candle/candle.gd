ee97df438c305b2940e08224b495dc547cf0acf3
gizmolo <jacob.wallace@utexas.edu>
2024-04-13 19:13:42 -0500

    hardcoding sigil sequences

--- objects/summoning_circle/candle/candle.gd
+++ objects/summoning_circle/candle/candle.gd
@@ -1,6 +1,7 @@
 extends Node2D
 var summoning_circle_ref: Node2D
-
+var active = false
+var index = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,14 +14,17 @@ func _process(delta):
 
 func activate_candle():
 	$CandleLightSprite.frame = 1
+	active = true
 
 func deactivate_candle():
 	$CandleLightSprite.frame = 0
+	active = false
 
 func _on_body_entered(body):
 	if body.name == "Player" and Input.is_action_pressed("Summon"):
 		if Globals.summon_ink > 0:
 			summoning_circle_ref.sigil_sequence_active = true
+			summoning_circle_ref.push_active_candle(index)
 			Globals.summon_ink = 100
 			activate_candle()
 			# signal to summoning circle
