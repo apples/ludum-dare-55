95f915beffb15c74857419322439e31c9d6262e3
WiredOverload <mike.everywhere@gmail.com>
2024-04-13 20:59:01 -0500

    healthbar color

--- ui/health_bar.gd
+++ ui/health_bar.gd
@@ -4,4 +4,6 @@ func _ready() -> void:
 	Globals.changed.connect(_on_possible_change)
 	
 func _on_possible_change():
-	scale = Vector2(Globals.boss_health as float / Globals.boss_max_health as float, 1)
+	var ratio : float = float(Globals.boss_health) / float(Globals.boss_max_health)
+	scale = Vector2(ratio, 1)
+	color = Color((1 - ratio) * 0.733, ratio * 0.733, 0)
