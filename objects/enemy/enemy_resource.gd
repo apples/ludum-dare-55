136609fc96414ef8c08e4c8c9662500b7634fc24
xxAtrain223 <xxAtrain223@users.noreply.github.com>
2024-04-14 18:10:54 -0500

    Added enemy_resource and shooting enemy.

---
+++ objects/enemy/enemy_resource.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name EnemyResource
+
+@export var texture: Texture2D
+@export var element: Globals.Elements
+@export var health: float
+@export var score_value: float
+@export var bullet_resource: BulletResource
+@export var initial_shoot_delay: float
+@export var shoot_cooldown: float
