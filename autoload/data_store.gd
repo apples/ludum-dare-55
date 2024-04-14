c0076e5d3e09293ffb49e2977a7f9e635f96f992
Charlie Hume <charliehume33@gmail.com>
2024-04-14 14:02:19 -0500

    chume: Options Menu and initial background music

---
+++ autoload/data_store.gd
@@ -0,0 +1,39 @@
+extends Node
+
+const filename = "user://fishermans_wife.json"
+
+var current: Dictionary
+
+var _default_values := {
+	highscore = 0,
+	max_level_reached = 0,
+	master_volume = 0.5,
+	sfx_volume = 1,
+	music_volume = 1,
+	hard_mode_unlocked = false,
+}
+
+func _ready():
+	reload()
+
+func save():
+	var data = JSON.stringify(current)
+	var file = FileAccess.open(filename, FileAccess.WRITE)
+	file.store_string(data)
+	file.close()
+	print("Game saved")
+
+func reload():
+	var file = FileAccess.open(filename, FileAccess.READ)
+	if not file:
+		current = _default_values.duplicate(true)
+		print("No save game found")
+		return
+	var data = file.get_as_text()
+	current = JSON.parse_string(data)
+	
+	print("Loaded found save game")
+
+func delete_data():
+	current = _default_values.duplicate(true)
+	save()
